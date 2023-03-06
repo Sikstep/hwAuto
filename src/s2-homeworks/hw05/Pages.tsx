a3721ad5c5791330601024ba56a26a4b7f9ac465
Sikstep <Sikstep1988@gmail.com>
2023-03-06 22:31:09 +0300

    AutoHW - #5 started+

--- src/s2-homeworks/hw05/Pages.tsx
+++ src/s2-homeworks/hw05/Pages.tsx
@@ -4,6 +4,7 @@ import Error404 from './pages/Error404'
 import PreJunior from './pages/PreJunior'
 import Junior from './pages/Junior'
 import JuniorPlus from './pages/JuniorPlus'
+import {Sidebar} from './sidebar/Sidebar';
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
@@ -15,18 +16,19 @@ function Pages() {
     return (
         <div>
             {/*Routes выбирает первый подходящий роут*/}
+
             <Routes>
                 {/*роутинг будут писать студенты*/}
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу /pre-junior*/}
-                {/*<Route ...*/}
+                <Route path={'/'} element={<PreJunior/>}/>
 
                 {/*роуты для /pre-junior, /junior, /junior-plus*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
-                {/*<Route ...*/}
+                <Route path={'/pre-junior'} element={<PreJunior/>}/>
+                <Route path={'/junior'} element={<Junior/>}/>
+                <Route path={'/junior-plus'} element={<JuniorPlus/>}/>
 
-                {/*роут для несуществующей страницы должен отрисовать <Error404 />*/}
-                {/*<Route ...*/}
+                {/*// роут для несуществующей страницы должен отрисовать <Error404 />*/}
+                <Route path={'/*'} element={<Error404/>}/>
             </Routes>
         </div>
     )
