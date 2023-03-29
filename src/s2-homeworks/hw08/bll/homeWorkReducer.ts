63fc39032d8dc8873ac4e61eacee75653a059374
Sikstep <Sikstep1988@gmail.com>
2023-03-29 18:03:31 +0300

    AutoHW - #8 started+

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,11 +4,17 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            let stateCopy = state.map(user => ({...user}))
+            if (action.payload == 'up') {
+                stateCopy = stateCopy.sort((a, b) => a.age - b.age)
+            } else {
+                stateCopy = stateCopy.sort((a, b) => b.age - a.age)
+            }
 
-            return state // need to fix
+            return stateCopy // need to fix
         }
         case 'check': {
 
