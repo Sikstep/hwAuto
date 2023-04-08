2af85d8ed71144f88aed03fdb75aeb9c8c2b2de5
Sikstep <Sikstep1988@gmail.com>
2023-04-08 16:37:41 +0300

    AutoHW - #9 stringDay completed

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -35,7 +35,7 @@ function Clock() {
     const stringDate = 'date->date' || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    const stringDay = '' || <br/> // пишут студенты
+    const stringDay = new Intl.DateTimeFormat('en-US', {weekday:'long'}).format(date) || <br/> // пишут студенты
     const stringMonth = 'date->month' || <br/> // пишут студенты
 
     return (
